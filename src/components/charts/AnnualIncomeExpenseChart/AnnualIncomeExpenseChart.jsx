37a9a03186c507878b5cefa9a1ccae39fff71977
suld2495 <suld7777@gmail.com>
2023-11-11 18:15:25 +0900

    각 컴포넌트로 데이터 전달

--- src/components/charts/AnnualIncomeExpenseChart/AnnualIncomeExpenseChart.jsx
+++ src/components/charts/AnnualIncomeExpenseChart/AnnualIncomeExpenseChart.jsx
@@ -3,20 +3,36 @@ import AnnualTotal from '../AnnualTotal/AnnualTotal';
 import ChartRadio from '../ChartRadio/ChartRadio';
 import IncomeChart from '../IncomeChart/IncomeChart';
 import OutcomeChart from '../OutcomeChart/OutcomeChart';
-import { useState } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-const AnnualIncomeExpenseChart = () => {
-  const [chartType, setChartType] = useState('bar');
+const AnnualIncomeExpenseChart = ({ item, dateState, setDateState }) => {
+  const [chartType, setChartType] = React.useState('bar');
+  const [yearItems, setYearItems] = React.useState([]);
+
+  React.useEffect(() => {
+    const year = dateState || new Date().getFullYear();
+    const newItems = item
+      .filter(({ date }) => date.getFullYear() === year);
+    setYearItems(newItems);
+  }, [item, dateState]);
+
   return (
     <div className={styled.annual}>
-      <AnnualTotal />
+      <AnnualTotal item={yearItems} dateState={dateState} setDateState={setDateState} />
       <ChartRadio chartType={chartType} setChartType={setChartType} />
       <div className={`${chartType === 'pie' ? styled["pie-chart"] : ''}`}>
         <IncomeChart chartType={chartType} />
-        <OutcomeChart chartType={chartType} />
+        <OutcomeChart chartType={chartType} item={yearItems} />
       </div>
     </div>
   )
 };
+
+AnnualIncomeExpenseChart.propTypes = {
+  item: PropTypes.array,
+  dateState: PropTypes.string,
+  setDateState: PropTypes.func,
+}
 
 export default AnnualIncomeExpenseChart;
