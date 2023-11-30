56f081fc441c307350aa8d3ec74dc586d40963bd
gayoung0619 <did-619@naver.com>
2023-11-30 16:19:53 +0900

    페이지네이션 오류수정 (해당페이지그룹에서 데이터가 있는 페이지까지만 노출)

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,15 +2,23 @@ import PropTypes from "prop-types";
 import styled from "./Pagination.module.css";
 import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
 const Pagination = ({total, postPerPage, setCurrentPage, currentPage}) => {
-	// 페이지당 3개씩 보여지도록 처리예정 -> 4 / 3 = 1.333 -> 2로 올림 - js
-	const endPage =  Math.ceil(total / postPerPage);
+
+	// 페이지당 3개씩 보여지도록 처리예정 -> 17 / 3 = 5.666 -> 6로 올림 - js
+	const endPage =  Math.ceil(total / postPerPage); // 마지막 페이지
 	const pageGroupSize = 5; // 한 그룹당 표시할 페이지 개수
 	// 1/5, 2/5, 3/5, 4/5, 5/5 -> 그룹 1
 	// 6/5, 7/5, 8/5, 9/5, 10/5 -> 그룹 2
 	const currentPageGroup = Math.ceil(currentPage / pageGroupSize);
-	const startPage = Math.max((currentPageGroup - 1) * pageGroupSize + 1, 1);
-	const endPageGroup = Math.min(startPage + pageGroupSize - 1, endPage);
-	const pageNumbers = Array.from({ length: endPageGroup - startPage + 1 }, (_, index) => startPage + index);
+	// 현재 페이지가 속한 페이지 그룹의 첫번째페이지넘버
+	const startPageNum = (currentPageGroup - 1) * pageGroupSize + 1;
+	// 현재 페이지가 속한 페이지 그룹의 마지막페이지넘버
+	const endPageNum = Math.min(startPageNum + pageGroupSize - 1, endPage);
+  // 현재 페이지 그룹에서 보여질 페이지 번호를 나타내는 배열
+	//const pageNumbers = Array.from({ length: endPageNum - startPageNum + 1 }, (_, index) => startPageNum + index);
+	const pageNumbers = [];
+	for (let i = startPageNum; i <= endPageNum; i++) {
+		pageNumbers.push(i);
+	}
 
 	const pageClick = (data) => {
 		setCurrentPage(data);
@@ -48,12 +56,16 @@ const Pagination = ({total, postPerPage, setCurrentPage, currentPage}) => {
 	return (
 		<div className={styled["pagination"]}>
 			<div className={styled["pagination__content"]}>
-				{/*<div>페이지당 갯수: {postPerPage}</div>*/}
-				{/*<div>총 글 갯수: {total}</div>*/}
+				<div>총 글 갯수: {total}</div>
 				<div>
 					<SlArrowLeft className={styled["pagination__prev"]} onClick={prePage} />
 					{result}
 					<SlArrowRight className={styled["pagination__next"]} onClick={nextPage} />
+				</div>
+				<div className={styled["pagination__current"]}>
+					<span>{currentPage}</span>
+					/
+					<span>{endPage}</span>
 				</div>
 			</div>
 		</div>
