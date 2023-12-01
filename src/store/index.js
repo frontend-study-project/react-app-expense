43387e32d86bf678043f866c6098e4d79dde0dec
suld2495 <suld7777@gmail.com>
2023-12-01 22:58:47 +0900

    feat: 차트 상태 관리용 리듀서 생성

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import formReducer from './form.js';
+import chartReducer from './chart.js';
 
 export const store = configureStore({
   reducer: {
-    form: formReducer
+    form: formReducer,
+    chart: chartReducer,
   },
 });
