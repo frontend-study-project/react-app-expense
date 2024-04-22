e3d6b7ac9dc8c94d00d0c96b8da34567abf2b412
yeeun <84173008+jye104@users.noreply.github.com>
2024-04-22 18:08:44 +0900

    Feat: typeReducer 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import formReducer from './form.js';
 import chartReducer from './chart.js';
+import typeReducer from './type.js';
 
 export const store = configureStore({
   reducer: {
     form: formReducer,
     chart: chartReducer,
+    type: typeReducer,
   },
 });
