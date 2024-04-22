fe214b48630dbf53a8cd8d8afdfd299dc9840184
yeeun <84173008+jye104@users.noreply.github.com>
2024-04-22 18:07:06 +0900

    Fix: title->content 수정

--- src/store/items.js
+++ src/store/items.js
@@ -1,49 +1,49 @@
 // src/redux/slices/itemsSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-	{
-		id: "e1",
-		type: "outcome", // type 추가
-		category: "쇼핑",
-		title: "Toilet Paper",
-		amount: 94.12,
-		date: new Date(2020, 7, 14),
-	},
-	{
-		id: "e2",
-		type: "outcome",
-		category: "쇼핑",
-		title: "New TV",
-		amount: 799.49,
-		date: new Date(2021, 2, 12),
-	},
-	{
-		id: "e3",
-		type: "outcome",
-		category: "보험",
-		title: "Car Insurance",
-		amount: 294.67,
-		date: new Date(2021, 2, 28),
-	},
-	{
-		id: "e4",
-		type: "outcome",
-		category: "쇼핑",
-		title: "New Desk (Wooden)",
-		amount: 450,
-		date: new Date(2021, 5, 12),
-	}
+  {
+    id: "e1",
+    type: "outcome", // type 추가
+    category: "쇼핑",
+    content: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    type: "outcome",
+    category: "쇼핑",
+    content: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    type: "outcome",
+    category: "보험",
+    content: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    type: "outcome",
+    category: "쇼핑",
+    content: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
 ];
 
 const reducers = {
-	setItems: (state, action) => action.payload
+  setItems: (state, action) => action.payload,
 };
 
 const itemsSlice = createSlice({
-	name: 'items',
-	initialState,
-	reducers
+  name: "items",
+  initialState,
+  reducers,
 });
 
 export const { setItems } = itemsSlice.actions;
