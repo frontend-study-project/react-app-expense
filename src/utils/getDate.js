053605b68fee1c2311646e6d5e826d61d5b747d2
yeeun <84173008+jye104@users.noreply.github.com>
2024-04-22 17:58:18 +0900

    Feat: date 가져오는 함수를 util로 빼내기

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -1,3 +1,7 @@
+// components는 화면에 뿌려지는 요소들이고 utils은 자주 쓰는 기능들(함수) 공통화시켜서 사용
+// hooks은 react에서만 사용, utils은 아무데서나..(javascript...)
+// => 불필요한, 반복적인 사용을 줄이기 위해 사용
+
 export const getDate = (date) => {
   const year = date.getFullYear();
   const day = date.getDate().toString().padStart(2, "0");
